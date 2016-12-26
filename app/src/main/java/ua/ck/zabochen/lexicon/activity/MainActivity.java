ce53dd8c20557a60a7eb6dc19a9986438a47b7f0
zabochen <zabochen@gmail.com>
2016-12-27 00:02:01 +0200

    Added BottomNavigationView. Functional not implemented.

--- app/src/main/java/ua/ck/zabochen/lexicon/activity/MainActivity.java
+++ app/src/main/java/ua/ck/zabochen/lexicon/activity/MainActivity.java
@@ -1,14 +1,17 @@
 package ua.ck.zabochen.lexicon.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import ua.ck.zabochen.lexicon.R;
 import ua.ck.zabochen.lexicon.fragment.WordsFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -27,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         toolbar.setTitleTextColor(ContextCompat.getColor(getApplicationContext(), R.color.toolbar_title));
         setSupportActionBar(toolbar);
+
+        // BottomNavigation View
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.activity_main_bottom_navigation_view);
+        bottomNavigationView.setOnClickListener(this);
     }
 
     private void setFragment(Bundle bundle) {
@@ -35,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
                     .beginTransaction()
                     .add(R.id.activity_main_frame_layout, new WordsFragment())
                     .commit();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.activity_main_bottom_navigation_view:
+                break;
         }
     }
 }
